f123b296ceb16572fb316b9a950c9beec3283e92
Shannon Hicks <shan@iotashan.com>
2012-08-26 18:42:28 -0500

    First commit of code.

---
+++ reusable-components/Resources/BeerRow.js
@@ -0,0 +1,54 @@
+/**
+ * @author Shannon Hicks
+ */
+
+// detect screen density once because Ti.Platform.displayCaps calls are expensive
+var retina = Ti.Platform.displayCaps.density == 'high';
+
+var BeerRow = function(beer) {
+	// create the Row UI
+	var row = Titanium.UI.createTableViewRow({
+		className:'beerRow',
+		height:70,
+		data:beer
+	});
+
+	var beerImage = Titanium.UI.createImageView({
+		top:5,
+		left:5,
+		width:60,
+		height:60,
+		image:retina ? beer.image2x : beer.image,
+		hires:retina,
+	});
+	row.add(beerImage);
+	
+	var beerName = Titanium.UI.createLabel({
+		color:'#C36466',
+		font:{fontSize:14,fontWeight:'bold', fontFamily:'Arial'},
+		left:75,
+		right:30,
+		height:Ti.UI.SIZE,
+		text:beer.name
+	});
+	row.add(beerName);
+	
+	// setters & getters
+	var pushBeer = function(_beer){
+		// save the variable
+		beer = _beer;
+		
+		// update views
+		beerImage.image = retina ? beer.image2x : beer.image;
+		beerName.text = beer.name;
+	}
+	
+	var pullBeer = function() {
+		// return the variable
+		return beer;
+	}
+	
+	return row;
+}
+
+module.exports = BeerRow;
---
+++ reusable-components/Resources/app.js
@@ -0,0 +1,70 @@
+// this sets the background color of the master UIView (when there are no windows/tab groups on it)
+Titanium.UI.setBackgroundColor('#000');
+
+//
+// include our reusable component
+//
+var BeerRow = require('BeerRow');
+
+//
+// create root window
+//
+var win = Titanium.UI.createWindow({  
+    title:'My Table',
+    backgroundColor:'#fff'
+});
+
+//
+// stub some data
+//
+var beers = [
+	{
+		name:'Samuel Adams Utopias',
+		image:'http://beerimages.pintley.com/4078/samadamsutopias_mobileThumb.png',
+		image2x:'http://beerimages.pintley.com/4078/samadamsutopias_web.png',
+	},
+	{
+		name:'Samuel Adams Black & Brew Coffee Stout',
+		image:'http://beerimages.pintley.com/21939/ee22c5f5%2D357d%2D404a%2D84fe%2Dffbeb85bd737_mobileThumb.png',
+		image2x:'http://beerimages.pintley.com/21939/ee22c5f5%2D357d%2D404a%2D84fe%2Dffbeb85bd737_web.png',
+	},
+	{
+		name:'Samuel Adams The Vixen',
+		image:'http://beerimages.pintley.com/21354/Vixen%5FBottle_mobileThumb.png',
+		image2x:'http://beerimages.pintley.com/21354/Vixen%5FBottle_web.png',
+	},
+	{
+		name:'Samuel Adams Norse Legend',
+		image:'http://beerimages.pintley.com/23476/Norse%2DLegend%2DBody%2DLabel%2D575_mobileThumb.png',
+		image2x:'http://beerimages.pintley.com/23476/Norse%2DLegend%2DBody%2DLabel%2D575_web.png',
+	},
+];
+
+//
+// create table and add it to window
+//
+var beerTable = Ti.UI.createTableView({
+	top:0,
+	bottom:0,
+	left:0,
+	right:0,
+});
+
+win.add(beerTable);
+win.open();
+
+var tableRows = [];
+
+//
+// create our rows
+//
+for (var i in beers) {
+	var newRow = new BeerRow(beers[i]);
+	tableRows.push(newRow);
+}
+
+//
+// add our rows to the table
+//
+beerTable.data = tableRows;
+
